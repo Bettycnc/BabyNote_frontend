9b4f2d2d1856634aaf575af71718c09c696fd762
Kévin <k.schmitt.graph@gmail.com>
2025-03-11 17:10:13 +0100

    search bar + supprimer patiente depuis le menu

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -4,17 +4,30 @@ import React, { useState } from "react";
 const SearchBar = () => {
   const [search, setSearch] = useState("");
 
+  const names = ["Alice", "Bob", "Charlie", "Diana"];
+  const filteredNames = names.filter((name) =>
+    name.toLowerCase().includes(search.toLowerCase())
+  );
+  console.log(search);
+
   return (
-    <div className={styles.search}>
-      <input
-        type="text"
-        className={styles.input}
-        placeholder="Rechercher une patiente"
-        id="searchPatient"
-        onChange={(e) => setSearch(e.target.value)}
-        value={search}
-      />
-      <img src="/loupe.svg" alt="Menu" className={styles.icon} />
+    <div>
+      <div className={styles.search}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Rechercher une patiente"
+          id="searchPatient"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <img src="/loupe.svg" alt="Menu" className={styles.icon} />
+      </div>
+      <ul>
+        {filteredNames.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
